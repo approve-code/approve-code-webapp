6e9267de7bd0465ac0ea5c7b4cb3be646f701451
Ilya Antipenko <ilya@antipenko.pp.ua>
2015-12-01 00:41:43 +0200

    Remove obsolete compiler pass

--- src/ApproveCode/Bundle/ApiBundle/ApproveCodeApiBundle.php
+++ src/ApproveCode/Bundle/ApiBundle/ApproveCodeApiBundle.php
@@ -2,19 +2,9 @@
 
 namespace ApproveCode\Bundle\ApiBundle;
 
-use ApproveCode\Bundle\ApiBundle\DependencyInjection\Compiler\GithubParametersMapperPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApproveCodeApiBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new GithubParametersMapperPass());
-    }
 }
